76295c4f67b25a437cb51474da39e2e07e2e936b
Brendan Saliba <37648354+brendansaliba@users.noreply.github.com>
2019-04-20 14:05:23 -0700

    Initial commit

---
+++ alexander/Assets/Scripts/Player_Score.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player_Score : MonoBehaviour
+{
+
+    public float timeLeft = 120;
+    public int playerScore = 0;
+    public GameObject timeLeftUI;
+    public GameObject playerScoreUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timeLeftUI.gameObject.GetComponent<Text>().text = ("Time Left: " + (int)timeLeft);
+        playerScoreUI.gameObject.GetComponent<Text>().text = ("Scrore: " + (int)playerScore);
+        if (timeLeft < 0.1)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D trigger)
+    {
+        if (trigger.gameObject.tag == "EndLevel")
+        {
+            CountScore();
+        }
+
+        if (trigger.gameObject.tag == "coin")
+        {
+            playerScore = playerScore + 1000;
+            Destroy(trigger.gameObject);
+        }
+        
+    }
+
+    void CountScore()
+    {
+        playerScore = playerScore + (int)(timeLeft*10);
+        Debug.Log(playerScore);
+    }
+}
---
+++ alexander/Assets/Scripts/player_move_prot.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move_prot : MonoBehaviour
+{
+
+    public int playerSpeed = 10;
+    private bool facingRight = false;
+    public int playerJumpPower = 1250;
+    private float moveX;
+    public bool isGrounded;
+    public int bouncePower = 1500;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+        PlayerRaycast();
+    }
+
+    void PlayerMove()
+    {
+        //controls
+        moveX = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            Jump();
+        }
+
+        //animation
+
+        //player direction
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+
+        else if (moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+
+        //physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        //jumping code
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+        isGrounded = false;
+
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void PlayerRaycast()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
+        if (hit.distance < 0.9f && hit.collider.tag == "enemy")
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x, 0);
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * bouncePower);
+
+            Destroy(hit.collider.gameObject);
+        }
+    }
+
+}
